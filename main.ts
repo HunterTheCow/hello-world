40952a4a424af82e560ac9bbc318d732e6e85ac6
HunterTheCow <122539615+HunterTheCow@users.noreply.github.com>
2023-01-13 08:44:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("G A B C5 C D E F ", 60)
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("C E G B C E G B ", 120)
+    basic.showIcon(IconNames.SmallHeart)
+})
+basic.showIcon(IconNames.Yes)
+music.playMelody("C D E F G A B C5 ", 200)
+basic.forever(function () {
+	
+})
